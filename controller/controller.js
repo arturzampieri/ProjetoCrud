13c39c32bc411c631228b3fee827860519299f94
arturzampieri <arturzampieri@hotmail.com>
2016-06-28 10:40:46 -0300

    Add files via upload

---
+++ controller/controller.js
@@ -0,0 +1,41 @@
+//Criação de uma variavel e requisição da model
+var userModel = require('../models/models.js');
+//POST - Cadastra Usuário (Nome/Email /DDD /Telefone)
+exports.cadastraUsuario = function (req, res) {
+ 'use strict';
+var data = new userModel({
+ nome: req.body.nome,
+ email: req.body.email,
+ ddd: req.body.ddd,
+ telefone: req.body.telefone
+});
+
+ data.save(function(err, data) {
+  res.status(200).jsonp(data);
+ });
+};
+//Fim POST
+
+//GET - Procura todos os registros no banco de dados
+exports.consultaUsuarioCadastrado = function(req, res) {
+    'use strict'
+ userModel.find(function(err, data) {
+ res.status(200).jsonp(data);
+});
+};
+//Fim GET
+
+//DELETE - Procura Pelo ID e Deleta Tratando Erros 
+exports.deletaUsuarioById = function(req, res) {
+    'use strict'
+ userModel.findById({_id:req.params.id}, function(err, result){
+  if(!result){
+   res.status(422).jsonp({status:422,message:'Id não encontrado na base!'});
+  }else{
+   userModel.remove({_id:req.params.id}, function(err, result){
+    res.status(200).jsonp({status:200,message:'Usuário Deletado com Sucesso'});
+   })
+  }
+ })
+ //Fim Delete
+};
---
+++ validation/signUpValidation.js
@@ -0,0 +1,7 @@
+// module.exports = function(req,res){
+// var utils = require('./utils/')
+//  req.nome
+//  req.email
+//  req.senha
+//  req.telefone[{numero:'',ddd:''},{numero:'',ddd:''},{numero:'',ddd:''}]
+// };
