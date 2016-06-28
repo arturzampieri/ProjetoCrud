620e827d1ff486dcecb7f8583e0c5473f97de428
arturzampieri <arturzampieri@hotmail.com>
2016-06-28 10:39:56 -0300

    Delete signUpValidation.js

--- signUpValidation.js
+++
@@ -1,7 +0,0 @@
-// module.exports = function(req,res){
-// var utils = require('./utils/')
-//  req.nome
-//  req.email
-//  req.senha
-//  req.telefone[{numero:'',ddd:''},{numero:'',ddd:''},{numero:'',ddd:''}]
-// };
